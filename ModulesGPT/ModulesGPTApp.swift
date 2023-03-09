2b4f90e673d08c5aa6e08dee165d0b1563e92fc7
John Kilima <jkbundala@gmail.com>
2023-03-09 12:32:05 +0300

    Initial Commit

---
+++ ModulesGPT/ModulesGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModulesGPTApp.swift
+//  ModulesGPT
+//
+//  Created by John Kilima on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ModulesGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
